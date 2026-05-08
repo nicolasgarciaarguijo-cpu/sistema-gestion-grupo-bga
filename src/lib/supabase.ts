3db1ab6f9118da97915193c9ac11de43f9d188fb
nicolasgarciaarguijo-cpu <nicolasgarciaarguijo@gmail.com>
2026-05-07 23:43:01 -0300

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ilmcvnnawzorvzpwpoes.supabase.coL";
+const supabaseUrl = "https://ilmcvnnawzorvzpwpoes.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsbWN2bm5hd3pvcnZ6cHdwb2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2OTQ3NTAsImV4cCI6MjA5MzI3MDc1MH0.HkjK126_E53lPN5aFDO_MQpJCzeKrnfD1BN7LU4irhs";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage:
+      typeof window !== "undefined" && window.sessionStorage
+        ? window.sessionStorage
+        : undefined,
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
